a35f355b452bb9d66d7188276ab758ceeaf919ca
Arthur Pushkin <arthur.pushkin@flatsoft.com>
2011-12-09 07:43:25 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkshopsRor::Application.config.secret_token = '0629f25c245531a664c65377c1effce6a9ff6a0b9b7d2084076367b8aabf041ad15e902069119dd2d4634627038cb11ae59e3bfd36cc82e00a5735bd00a9f5cc'
