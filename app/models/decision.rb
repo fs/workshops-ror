9e6acd176a56dacc8ce286a358c6b76696d0d466
Ruslan Gatiyatov <ruslan.gatiyatov@gmail.com>
2011-12-09 17:53:09 +0400

    Add validations for decision model

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -1,2 +1,5 @@
 class Decision < ActiveRecord::Base
+  validates :subject, :description, presence: true
+  validates :subject, legth: {minimun: 8}
+  validates :description, legth: {minimun: 15}
 end
