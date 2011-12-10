5feafd1cb61e0838de48b2d9293d98d96a5d5844
Ruslan Gatiyatov <ruslan.gatiyatov@gmail.com>
2011-12-10 18:39:44 +0400

    Add initials to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   has_many :votes
   has_many :decisions
   has_many :messages
+  validates :first_name, :last_name, presence: true, length: {minimum: 3, maximum: 12}
+
+  def fullname
+    [first_name, last_name].reject(&:blank?).join(' ')
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111209180245) do
+ActiveRecord::Schema.define(:version => 20111210143844) do
 
   create_table "choices", :force => true do |t|
     t.string   "title"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(:version => 20111209180245) do
     t.text     "bio"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   create_table "votes", :force => true do |t|
