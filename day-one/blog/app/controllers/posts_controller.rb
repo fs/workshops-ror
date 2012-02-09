b0e8d053aed584bdff5ecc37cb6725d9d950aa66
arthrrr <arthrrr@gmail.com>
2012-02-09 15:57:28 +0400

    added Posts#show action; posts/show view

--- day-one/blog/app/controllers/posts_controller.rb
+++ day-one/blog/app/controllers/posts_controller.rb
@@ -2,4 +2,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.order('created_at DESC')
   end
+
+  def show
+  end
 end
--- day-one/blog/config/routes.rb
+++ day-one/blog/config/routes.rb
@@ -1,5 +1,6 @@
 Blog::Application.routes.draw do
   root :to => 'posts#index'
+  match 'posts/:id' => 'posts#show', :as => :post
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
