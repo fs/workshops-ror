ba862491fe8337bd2c28eed589df5cb239808171
Ruslan Gatiyatov <ruslan.gatiyatov@gmail.com>
2012-03-15 10:37:31 +0400

    Comment controller: create action

--- day-one/blog/app/controllers/comments_controller.rb
+++ day-one/blog/app/controllers/comments_controller.rb
@@ -3,4 +3,15 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build
   end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(params[:comment])
+    if @comment.save
+      flash[:notice] = 'Comment was successfully created'
+      redirect_to @post
+    else
+      render :new
+    end
+  end
 end
