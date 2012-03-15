279e8c51e09f454c3ff9473ddc816c5af3be16ce
Ruslan Gatiyatov <ruslan.gatiyatov@gmail.com>
2012-03-15 10:13:18 +0400

    Create comments: rails g model comment body:string post_id:integer

--- day-one/blog/db/schema.rb
+++ day-one/blog/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120209111750) do
+ActiveRecord::Schema.define(:version => 20120315060630) do
+
+  create_table "comments", :force => true do |t|
+    t.string   "body"
+    t.integer  "post_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
